5bb2a74de5c8883288e9060640ce662e12fa445b
sema8899 <namlisema77@gmail.com>
2025-11-02 14:44:25 +0300

    Add files via upload

---
+++ ders1.py
@@ -0,0 +1,10 @@
+sayi1 = int(input("Birinci sayıyı girin: "))
+sayi2 = int(input("İkinci sayıyı girin: "))
+
+toplam = sayi1 + sayi2
+fark = sayi1 - sayi2
+carpim = sayi1 * sayi2
+
+print("Toplam:", toplam)
+print("Fark:", fark)
+print("Çarpım:", carpim)
