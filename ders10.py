beb7dca08c2e119c6089ebbe2a269ed06270df8f
sema8899 <namlisema77@gmail.com>
2025-11-02 15:02:20 +0300

    Add files via upload

---
+++ ders10.py
@@ -0,0 +1,10 @@
+sayi1 = float(input("Birinci sayıyı girin: "))
+sayi2 = float(input("İkinci sayıyı girin: "))
+
+esit_mi = sayi1 == sayi2
+birinci_buyuk_mu = sayi1 > sayi2
+
+print("İki sayı eşit mi?", esit_mi)
+print("Birinci sayı ikinciden büyük mü?", birinci_buyuk_mu)
+
+
