34660269be00a3be2a75ba19a60fcc844b4a76d5
sema8899 <namlisema77@gmail.com>
2025-11-02 14:48:27 +0300

    Add files via upload

---
+++ ders3.py
@@ -0,0 +1,8 @@
+kisa_kenar = int(input("Kısa kenarı girin: "))
+uzun_kenar = int(input("Uzun kenarı girin: "))
+
+alan = kisa_kenar * uzun_kenar
+cevre = 2 * (kisa_kenar + uzun_kenar)
+
+print("Dikdörtgenin alanı:", alan)
+print("Dikdörtgenin çevresi:", cevre)
